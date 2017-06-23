d716679ce8a99d132d5356fa28ebbe3e543b36e0
daniel-m10 <daniel.montecinos@fundacion-jala.org>
2017-06-23 16:42:21 -0400

    Feature/hook improvement (#28)
    * deleteAllAccount method improvement
    
    * hook methods improvement
    
    * deleteAllAccount method improvement

--- src/test/java/org/fundacionjala/pivotal/cucumber/hooks/api/Hooks.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/hooks/api/Hooks.java
@@ -2,14 +2,11 @@ package org.fundacionjala.pivotal.cucumber.hooks.api;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.fundacionjala.pivotal.cucumber.api.RequestManager;
+import org.fundacionjala.pivotal.cucumber.utils.Utils;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import org.fundacionjala.pivotal.cucumber.api.RequestManager;
-import org.fundacionjala.pivotal.cucumber.utils.StoreVariables;
-import org.fundacionjala.pivotal.cucumber.utils.Utils;
 
 import static org.fundacionjala.pivotal.cucumber.api.RequestManager.delete;
 
@@ -34,14 +31,10 @@ public class Hooks {
      */
     @After("@deleteProject")
     public void setup() {
-        Iterator<StoreVariables> iter = Utils.getStoreVariables().iterator();
-        while (iter.hasNext()) {
-            StoreVariables variable = iter.next();
-            if (variable.getName().contains("Project")) {
-                delete(Utils.buildEndpoint(String.format("/projects/[%s.id]", variable.getName())));
-                iter.remove();
-            }
-        }
+        Utils.getStoreVariables()
+                .stream()
+                .filter(element -> element.getName().contains("Project"))
+                .forEach(project -> delete(Utils.buildEndpoint(String.format("/projects/[%s.id]", project.getName()))));
     }
 
     /**
@@ -57,14 +50,11 @@ public class Hooks {
      */
     @After("@deleteWorkspace")
     public void resetworkspace() {
-        Iterator<StoreVariables> iter = Utils.getStoreVariables().iterator();
-        while (iter.hasNext()) {
-            StoreVariables variable = iter.next();
-            if (variable.getName().contains("Workspace")) {
-                delete(Utils.buildEndpoint(String.format("/my/workspaces/[%s.id]", variable.getName())));
-                iter.remove();
-            }
-        }
+        Utils.getStoreVariables()
+                .stream()
+                .filter(element -> element.getName().contains("Workspace"))
+                .forEach(workspace ->
+                        delete(Utils.buildEndpoint(String.format("/my/workspaces/[%s.id]", workspace.getName()))));
     }
 
 
